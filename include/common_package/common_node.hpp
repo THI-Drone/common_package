4cd585146b8f93dcb27876fc77f97f9e608bebc9
Philipp66904 <philippg.pgb@gmail.com>
2024-04-09 23:00:32 +0200

    implemented common node with heartbeat

---
+++ include/common_package/common_node.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "interfaces/msg/Heartbeat.hpp"
+
+using namespace std::chrono_literals;
+
+/**
+ * @class CommonNode
+ * @brief A class representing a common node.
+ *
+ * This class inherits from rclcpp::Node and provides functionality for creating a node that sends heartbeat messages.
+ */
+class CommonNode : public rclcpp::Node
+{
+public:
+  /**
+   * @brief Constructor for creating a new CommonNode.
+   * @param id Unique name of the node.
+   */
+  CommonNode(char* id) : Node(id) {
+    this->id = id;
+
+    // Create a publisher for the "heartbeat" topic
+    publisher_ = this->create_publisher<interfaces::msg::Heartbeat>("heartbeat", 10);
+
+    // Create a timer that sends a heartbeat message every 500ms
+    timer_ = this->create_wall_timer(500ms, std::bind(&CommonNode::timer_callback, this));
+  }
+
+private:
+  /**
+   * @brief Callback function for the timer.
+   */
+  void timer_callback();
+
+  rclcpp::TimerBase::SharedPtr timer_;  ///< Timer for sending heartbeat messages
+  rclcpp::Publisher<interfaces::msg::Heartbeat>::SharedPtr publisher_;  ///< Publisher for the "heartbeat" topic
+  uint32_t tick_ = 0;  ///< Tick counting upwards with every heartbeat
+  char* id;  ///< Unique name of the node
+};
---
+++ src/common_node.cpp
@@ -0,0 +1,18 @@
+#include "common_package/common_node.hpp"
+
+
+/**
+ * @brief Callback function for the timer.
+ * 
+ * This function is called when the timer expires. It creates a Heartbeat message,
+ * sets the necessary fields, and publishes the message using the publisher.
+ * 
+ * @return void
+ */
+void CommonNode::timer_callback() {
+    auto message = interfaces::msg::Heartbeat();
+    message.sender_id = id;
+    message.active = false;
+    message.tick = ++tick_;
+    publisher_->publish(message);
+}
