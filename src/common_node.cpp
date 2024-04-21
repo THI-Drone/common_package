dc8f9f91ff7c97e7750c31657fd89aea18bb37ba
Philipp66904 <philippg.pgb@gmail.com>
2024-04-21 12:13:39 +0200

    changed empty string to valid empty json

--- src/common_node.cpp
+++ src/common_node.cpp
@@ -83,7 +83,7 @@ void CommonNode::job_finished()
 
     msg.sender_id = this->get_fully_qualified_name();
     msg.error_code = EXIT_SUCCESS; // set error code to 0 to indicate success
-    msg.payload = "";              // payload is empty
+    msg.payload = "{}";              // payload is empty
 
     job_finished_publisher->publish(msg);
 
