9905a05021f2c253d0ce23f0a31e0775e64e5c9d
Philipp66904 <philippg.pgb@gmail.com>
2024-04-21 12:32:07 +0200

    test now runs in its own domain

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -2,11 +2,17 @@
 
 #include "rclcpp/node.hpp"
 
+#define TEST_DOMAIN_ID 2
+
 int main(int argc, char **argv)
 {
-    rclcpp::init(0, nullptr);
+    rclcpp::InitOptions init_options;
+    init_options.set_domain_id(TEST_DOMAIN_ID);
+    rclcpp::init(0, nullptr, init_options);
+
     testing::InitGoogleTest(&argc, argv);
     const int ret = RUN_ALL_TESTS();
+
     rclcpp::shutdown();
     return ret;
 }
