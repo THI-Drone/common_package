5a3d786e5cb74c6301cb538df0d22f67a90f9da9
J0las <52259564+J0las@users.noreply.github.com>
2024-05-17 19:42:51 +0200

    Change comment style

--- include/common_package/node_names.hpp
+++ include/common_package/node_names.hpp
@@ -9,23 +9,23 @@
 namespace common_lib::node_names {
 
 constexpr char const *const MISSION_CONTROL =
-    "mission_control"; /**< Node name for the mission control node */
+    "mission_control";  //!< Node name for the mission control node
 
 constexpr char const *const WAYPOINT =
-    "waypoint"; /**< Node name for the navigate to waypoint node */
+    "waypoint";  //!< Node name for the navigate to waypoint node
 
 constexpr char const *const QRCODE_DETECTION =
-    "qrcode_detection"; /**< Node name for the qr code detection node */
+    "qrcode_detection";  //!< Node name for the qr code detection node
 
 constexpr char const *const FCC_BRIDGE =
-    "fcc_bridge"; /**< Node name for the FCC bridge node */
+    "fcc_bridge";  //!< Node name for the FCC bridge node
 
 constexpr char const *const TELEMETRY =
-    "telemetry"; /**< Node name for the telemetry node */
+    "telemetry";  //!< Node name for the telemetry node
 
 const std::set<std::string> VALID_CONTROL_NODE_NAMES{
     MISSION_CONTROL, WAYPOINT,
-    QRCODE_DETECTION}; /**< Names of nodes that are allowed to be passed control
-                        */
+    QRCODE_DETECTION};  //!< Names of nodes that are allowed to be passed
+                        //!< control
 
 }  // namespace common_lib::node_names
