ebb62d7bcf72994c0dc289500b5e7dc16ecba0a4
J0las <52259564+J0las@users.noreply.github.com>
2024-05-17 19:49:35 +0200

    Change qr code detection to qr code scanner

--- include/common_package/node_names.hpp
+++ include/common_package/node_names.hpp
@@ -14,8 +14,8 @@ constexpr char const *const MISSION_CONTROL =
 constexpr char const *const WAYPOINT =
     "waypoint";  //!< Node name for the navigate to waypoint node
 
-constexpr char const *const QRCODE_DETECTION =
-    "qrcode_detection";  //!< Node name for the qr code detection node
+constexpr char const *const QRCODE_SCANNER =
+    "qr_code_scanner_node";  //!< Node name for the qr code scanner node
 
 constexpr char const *const FCC_BRIDGE =
     "fcc_bridge";  //!< Node name for the FCC bridge node
@@ -25,7 +25,7 @@ constexpr char const *const TELEMETRY =
 
 const std::set<std::string> VALID_CONTROL_NODE_NAMES{
     MISSION_CONTROL, WAYPOINT,
-    QRCODE_DETECTION};  //!< Names of nodes that are allowed to be passed
-                        //!< control
+    QRCODE_SCANNER};  //!< Names of nodes that are allowed to be passed
+                      //!< control
 
 }  // namespace common_lib::node_names
