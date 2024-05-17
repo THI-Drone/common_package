e15682726fe5e327d71cad3c63fc1dcbeab42afd
J0las <52259564+J0las@users.noreply.github.com>
2024-05-17 19:10:57 +0200

    Ensure that a Node using common node is not dinamically renamed

--- include/common_package/common_node.hpp
+++ include/common_package/common_node.hpp
@@ -36,6 +36,11 @@ class CommonNode : public rclcpp::Node {
     CommonNode(const std::string &id,
                const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
         : Node(id, options) {
+        if (this->get_name() != id) {
+            RCLCPP_FATAL(this->get_logger(),
+                         "Renaming of nodes at runtime is not supported!");
+            std::exit(EXIT_FAILURE);
+        }
         /// Create a publisher for the "heartbeat" topic
         heartbeat_publisher =
             this->create_publisher<interfaces::msg::Heartbeat>(
